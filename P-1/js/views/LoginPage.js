fa1f5d46e524df517386c90dbd6313a641e585cc
jun1090 <abide0206@naver.com>
2023-09-04 15:13:53 +0900

    이게 왜 되지?

--- P-1/js/views/LoginPage.js
+++ P-1/js/views/LoginPage.js
@@ -12,9 +12,12 @@ class LoginPage {
     //login page main 부분
     const login_main = document.createElement('div');
     login_main.setAttribute('class', 'login__main');
-    const login_input = document.createElement('input');
-    login_input.setAttribute('class', 'input-id');
-    login_input.placeholder = 'ID를 입력하시오';
+    const login_input = document.createElement('div');
+    login_input.setAttribute('class', 'inputBox');
+    const id_input = document.createElement('input');
+    const password_input = document.createElement('input');
+    id_input.placeholder = 'ID를 입력하시오';
+    password_input.placeholder = '비밀번호를 입력하시오';
     const buttonBox = document.createElement('div');
     buttonBox.setAttribute('class', 'twoBtnBox');
     const register_box = this.createBtnBox('회원가입', 'register-box');
@@ -26,6 +29,13 @@ class LoginPage {
     login_main.appendChild(buttonBox);
     buttonBox.appendChild(register_box);
     buttonBox.appendChild(login_box);
+    login_input.appendChild(id_input);
+    login_input.appendChild(password_input);
+
+    //회원가입 클릭 이벤트
+    login_input.addEventListener('click', () => {
+      console.log('print!');
+    });
   }
 }
 export default LoginPage;
